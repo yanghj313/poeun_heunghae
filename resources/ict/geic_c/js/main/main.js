010f088f2bd4493a394101be811d94dea3e9baa7
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-04 13:08:41 +0900

    [feat] 시간 요일 반환 함수 리팩토링

--- resources/ict/geic_c/js/main/main.js
+++ resources/ict/geic_c/js/main/main.js
@@ -1,18 +1,12 @@
 function updateDateTime() {
 	const now = new Date();
 
-	const hours = String(now.getHours()).padStart(2, '0');
-	const minutes = String(now.getMinutes()).padStart(2, '0');
-	document.getElementById('hours').textContent = hours;
-	document.getElementById('minutes').textContent = minutes;
+	document.getElementById('hours').textContent = String(now.getHours()).padStart(2, '0');
+	document.getElementById('minutes').textContent = String(now.getMinutes()).padStart(2, '0');
 
-	const year = now.getFullYear();
-	const months = String(now.getMonth() + 1).padStart(2, '0');
-	const days = String(now.getDate()).padStart(2, '0');
 	const week = ['일', '월', '화', '수', '목', '금', '토'];
-	const dayOfWeek = week[now.getDay()];
+	const formattedDate = `${now.getFullYear()}. ${String(now.getMonth() + 1).padStart(2, '0')}. ${String(now.getDate()).padStart(2, '0')}. ${week[now.getDay()]}요일`;
 
-	const formattedDate = `${year}. ${months}. ${days}. ${dayOfWeek}요일`;
 	document.getElementById('months').textContent = formattedDate;
 }
 
