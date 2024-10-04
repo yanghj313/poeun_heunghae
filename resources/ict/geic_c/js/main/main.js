7787813e24ab7744cd73d81ab16a9e8f311e6b3a
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-04 13:06:28 +0900

    [feat] 메인 퍼블리싱 완료 (폰트 변경 필요)

--- resources/ict/geic_c/js/main/main.js
+++ resources/ict/geic_c/js/main/main.js
@@ -6,14 +6,14 @@ function updateDateTime() {
 	document.getElementById('hours').textContent = hours;
 	document.getElementById('minutes').textContent = minutes;
 
-	const months = now.getMonth() + 1;
-	const days = now.getDate();
+	const year = now.getFullYear();
+	const months = String(now.getMonth() + 1).padStart(2, '0');
+	const days = String(now.getDate()).padStart(2, '0');
 	const week = ['일', '월', '화', '수', '목', '금', '토'];
 	const dayOfWeek = week[now.getDay()];
 
-	document.getElementById('months').textContent = months;
-	document.getElementById('days').textContent = days;
-	document.getElementById('dayofweeks').textContent = `${dayOfWeek}요일`;
+	const formattedDate = `${year}. ${months}. ${days}. ${dayOfWeek}요일`;
+	document.getElementById('months').textContent = formattedDate;
 }
 
 $(document).ready(function () {
