722a26b583def1bae8979ef1ba30ac468cc52bd9
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-07 11:15:05 +0900

    [feat] 네비게이션바 액티브 처리 추가

---
+++ resources/ict/geic_c/js/common/navigation.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  function toggleActiveMenu(clickedItem) {
+    $(".navigation li").each(function () {
+      $(this).find(".active-menu").removeClass("active-menu").addClass("menu");
+      $(this).find("span").removeClass("active-caption");
+
+      $(this)
+        .find("img.item")
+        .attr("src", function (index, src) {
+          return src.includes("active_") ? src.replace("active_", "") : src;
+        });
+    });
+
+    clickedItem.find(".menu").removeClass("menu").addClass("active-menu");
+    clickedItem.find("span").addClass("active-caption");
+
+    clickedItem.find("img.item").attr("src", function (index, src) {
+      return !src.includes("active_")
+        ? src.replace(/^(.*\/)(.*)$/, "$1active_$2")
+        : src;
+    });
+  }
+
+  $(".navigation li").on("click", function () {
+    toggleActiveMenu($(this));
+  });
+});
