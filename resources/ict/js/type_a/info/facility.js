800b47726158b64c2897043b1d24228dddb282e6
yanghj313 <lawork313@gmail.com>
2024-10-31 16:00:57 +0900

    [feat]  라이브러리보드  시설안내

--- resources/ict/js/type_a/info/facility.js
+++ resources/ict/js/type_a/info/facility.js
@@ -1,39 +1,85 @@
-//시설안내
 $(document).ready(function () {
-  $(".faci_tab a").click(function () {
-    var tab_id = $(this).attr("data-tab");
+	var swiper1 = new Swiper('.swiper1', {
+		loop: false,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		spaceBetween: 30,
+		speed: 1000,
+		on: {
+			reachEnd: function () {
+				setTimeout(function () {
+					$('.swiper1').fadeOut(500, function () {
+						$('.swiper2').fadeIn(500);
+						swiper2.slideTo(0);
+						$('.cate1').removeClass('on');
+						$('.cate2').addClass('on');
+					});
+				}, 1000);
+			},
+		},
+	});
 
-    $(".faci_tab a").removeClass("active");
-    $(".tab_content").removeClass("active");
+	var swiper2 = new Swiper('.swiper2', {
+		loop: false,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		spaceBetween: 30,
+		speed: 1000,
+		on: {
+			reachEnd: function () {
+				setTimeout(function () {
+					$('.swiper2').fadeOut(500, function () {
+						$('.swiper3').fadeIn(500);
+						swiper3.slideTo(0);
+						$('.cate2').removeClass('on');
+						$('.cate3').addClass('on');
+					});
+				}, 1000);
+			},
+		},
+	});
 
-    $(this).addClass("active");
-    $("#" + tab_id).addClass("active");
-  });
-});
-
-$(function () {
-  $(".floor_slide_01").bxSlider({
-    infiniteLoop: false,
-    controls: false,
-    pager: true,
-    slideMargin: 40,
-  });
-});
-
-$(function () {
-  $(".floor_slide_02").bxSlider({
-    infiniteLoop: false,
-    controls: false,
-    pager: true,
-    slideMargin: 40,
-  });
-});
-
-$(function () {
-  $(".floor_slide_03").bxSlider({
-    infiniteLoop: false,
-    controls: false,
-    pager: true,
-    slideMargin: 40,
-  });
+	var swiper3 = new Swiper('.swiper3', {
+		loop: false,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		spaceBetween: 30,
+		speed: 1000,
+		on: {
+			reachEnd: function () {
+				setTimeout(function () {
+					$('.swiper3').fadeOut(500, function () {
+						$('.swiper1').fadeIn(500);
+						swiper1.slideTo(0);
+						$('.cate3').removeClass('on');
+						$('.cate1').addClass('on');
+					});
+				}, 1000);
+			},
+		},
+	});
 });
